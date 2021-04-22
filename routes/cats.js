54fc4d7d35382bc3f0306ccd4700214424d695f4
zapatran <zapatran@users.noreply.github.com>
2021-04-22 21:06:15 +0200

    list all cats using mongoose

--- routes/cats.js
+++ routes/cats.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const Cat = require('../models/cat');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  const cats = [{ name: 'Garfield' }];
-  res.render('cats/list', { cats });
+router.get('/', (req, res, next) => {
+  // Cat.find({})
+  //   .then(cats => cats.map(cat => ({ name: cat.name.toUpperCase() }))
+  //   .then((cats) => {
+  //     res.render('cats/list', { cats });
+  //   })
+  //   .catch(error => {
+  //     next(error);
+  //   });
+  Cat.find({})
+    .then(cats => {
+      res.render('cats/list', { cats });
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;
