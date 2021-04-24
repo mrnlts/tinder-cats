8543b1f47ececd73cfba2e7a86a47d34db375a5f
zapatran <zapatran@users.noreply.github.com>
2021-04-24 11:27:31 +0200

    add create and delete

--- readme.md
+++ readme.md
@@ -4,5 +4,8 @@
 
 `GET /cats` -> list all cats
 `GET /cats/:id` -> cat detail
+
 `GET /cats/create` -> show the form for create a cat
 `POST /cats` -> create the cat
+
+`POST /cats/:id` -> delete the cat with that id
--- routes/cats.js
+++ routes/cats.js
@@ -22,11 +22,43 @@ router.get('/', (req, res, next) => {
     });
 });
 
+router.get('/create', (req, res) => {
+  res.render('cats/create');
+});
+
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
   Cat.findById(id)
     .then(cat => {
       res.render('cats/detail', { cat });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.post('/', (req, res, next) => {
+  const cat = req.body;
+  Cat.create({
+    name: cat.name,
+  })
+    .then(cat => {
+      console.log(cat);
+      // 💥 la url del redirect es completa no como los paths del middleware
+      res.redirect('/cats');
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.post('/:id', (req, res, next) => {
+  const { id } = req.params;
+  Cat.findByIdAndDelete(id)
+    .then(cat => {
+      console.log('delete', cat);
+      res.status(301);
+      res.redirect('/cats');
     })
     .catch(error => {
       next(error);
